97deab8f409d8f3d0d26dcea39481bf823f426b3
Chima Chukwuemeka <chukwuemekachm@gmail.com>
2019-04-29 17:43:38 +0100

    chore(react app): completes setup of react app
    - setup webpack
    - setup react app
    - setup styleguide-react
    - setup jest/enzyme
    - setup eslint
    - add a .travis config file
    
    [Maintains #1]

---
+++ src/components/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Visand-Prous ONLINE T-SHIRT SHOP</h1>
+      <p>
+        visand-prous is an  an online T-Shirt shop which allows
+        {' '}
+        users to search, add items to their shopping
+        {' '}
+        cart, create order and pay for their orders successfully.
+      </p>
+    </div>
+  );
+}
+
+export default App;
---
+++ webpack/webpack.common.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const DIST_DIR = path.join(__dirname, './dist');
+const SRC_DIR =  './src';
+
+module.exports = {
+  entry: `${SRC_DIR}/index.js`,
+  output: {
+    path: DIST_DIR,
+    filename: '[name].chunk.js',
+  },
+  resolve: {
+    extensions: ['.jsx', '.js'],
+    modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+    alias: {
+      _components: path.resolve(__dirname, 'src/components/'),
+      _atoms: path.resolve(__dirname, 'src/components/atoms/'),
+      _molecules: path.resolve(__dirname, 'src/components/molecules/'),
+      _organisms: path.resolve(__dirname, 'src/components/organisms/'),
+      _templates: path.resolve(__dirname, 'src/components/templates/'),
+    },
+  },
+  plugins: [
+    new webpack.ProgressPlugin(),
+    new HtmlWebpackPlugin({
+      template: `${SRC_DIR}/index.html`,
+      inject: 'body',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js?$|.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+            plugins: [
+              '@babel/plugin-proposal-object-rest-spread',
+              '@babel/plugin-transform-arrow-functions',
+            ],
+          },
+        },
+      },
+    ],
+  },
+};
